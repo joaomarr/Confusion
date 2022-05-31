d2ef966a3d27cd11831143eb901306a5df7ba5fe
João Vitor Marra <marrajoaosa@gmail.com>
2022-05-31 20:44:34 -0300

    first commit

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,94 @@
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+
+function RenderDish(dish) {
+  return (
+    <Card>
+      <CardImg top src={dish.image} alt={dish.name} />
+      <CardBody>
+        <CardTitle>{dish.name}</CardTitle>
+        <CardText>{dish.description}</CardText>
+      </CardBody>
+    </Card>
+  );
+}
+
+function RenderComments(comments) {
+  if (comments === null) {
+    return;
+  }
+
+  const listOfComments = comments.map((comment) => {
+    return (
+      <ul class="list-unstyled">
+        <li>{comment.comment}</li>
+        <li>
+          -- {comment.author},{" "}
+          {new Intl.DateTimeFormat("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "2-digit",
+          }).format(new Date(Date.parse(comment.date)))}
+        </li>
+      </ul>
+    );
+  });
+
+  return listOfComments;
+}
+
+const DishDetail = (props) => {
+  if (props.dish) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div key={props.dish.id} className="row">
+          <div className="col-12 col-md-5 m-1">{RenderDish(props.dish)}</div>
+          <div className="col-12 col-md-5 m-1">
+            <h4>Comments</h4>
+            {RenderComments(props.comments)}
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default DishDetail;
